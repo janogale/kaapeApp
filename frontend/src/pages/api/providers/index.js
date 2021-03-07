035d4fc8c5ceb8fba5d5cf8a322afadb6a504c5f
Hussein Ismail <hussein.ismail@eatech.fi>
2021-03-07 18:52:09 +0200

    Get service providers and short code for service provider

--- frontend/src/pages/api/providers/index.js
+++ frontend/src/pages/api/providers/index.js
@@ -1,18 +1,8 @@
-// Fake users data
-const serviceProviders = [
-  {
-    id: "a4337c7a-68b2-45f8-827d-8e4389d5eb51",
-    name: "Daryeel Hotel",
-  },
-  {
-    id: "a4337c7a-68b2-45f8-827d-8e4389d5eb66",
-    name: "Daryeel Hotel",
-  },
-];
+import { getServiceProviderList } from "../../../service/kaabe";
 
 // service providers route
 
-export default function handler(req, res) {
-  // Get data from your database
-  res.status(200).json(serviceProviders);
+export default async function handler(req, res) {
+  var providerList = await getServiceProviderList({});
+  res.status(200).json(providerList);
 }
