25a3d55bcebfb657f715be58ef06f53f0a05700a
Hussein Ismail <hussein.ismail@eatech.fi>
2021-03-19 16:27:34 +0200

    Remove console logs

--- frontend/src/pages/api/providers/index.js
+++ frontend/src/pages/api/providers/index.js
@@ -3,7 +3,6 @@ import { getServiceProviderList } from "../../../service/kaabe";
 // service providers route
 
 export default async function handler(req, res) {
-  console.log('getServiceProviderListxxx:', getServiceProviderList)
   var providerList = await getServiceProviderList();
   res.status(200).json(providerList);
 }
