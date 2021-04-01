c14f70091c721f22124ed168ef0f329c71701b08
Hussein Ismail <hussein.ismail@eatech.fi>
2021-04-01 12:11:09 +0300

    Update title and metadata

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -19,7 +19,7 @@ export const Header = (props) => {
       <DrawerLeft />
       <Heading as="h1" textAlign="center" fontSize="lg" flexGrow={2}>
         <Link href="/">
-          <a>Digital Menu</a>
+          <a>Kaabe Digital Waiter</a>
         </Link>
       </Heading>
     </Flex>
